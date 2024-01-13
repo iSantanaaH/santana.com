7b758619d5b6b434e96898610fa94f059aef78f1
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-13 11:34:55 -0300

    change icon and create folder images

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
+      <link rel="icon" href="/Images/shop.svg" />
       <body className={inter.className}>{children}</body>
     </html>
   );
