f8a9c2a0eb65f2dc13266c834a23467346ffc97c
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-13 12:48:49 -0300

    calling MainNavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import MainNavBar from "./Components/NavBar";
 
 export default function Home() {
-  return <main className={styles.MainHome}></main>;
+  return (
+    <main className={styles.MainHome}>
+      <section>
+        <MainNavBar />
+      </section>
+    </main>
+  );
 }
