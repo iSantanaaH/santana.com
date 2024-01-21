f2d70ae281c030b1b3324a77f3e0586483818566
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-21 15:42:30 -0300

    remove cadastro provider and import component providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-import { CadastroProvider } from "./context/cadastroContext";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <CadastroProvider>
-      <html lang="pt-br">
-        <link rel="icon" href="/Images/logo.svg" />
-        <body className={inter.className}>{children}</body>
-      </html>
-    </CadastroProvider>
+    <html suppressHydrationWarning lang="pt-br">
+      <link rel="icon" href="/Images/logo.svg" />
+      <body className={inter.className}>
+        <Providers>
+          <main>{children}</main>
+        </Providers>
+      </body>
+    </html>
   );
 }
