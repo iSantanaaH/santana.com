5f01770b15f06b1daea1b6a67ba5ae8544eea8f0
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-23 18:33:49 -0300

    update createUserController test

--- back-end_node/src/controllers/users/createUserController.ts
+++ back-end_node/src/controllers/users/createUserController.ts
@@ -2,17 +2,20 @@ import { Request, Response } from "express";
 
 const createUserController = (req: Request, res: Response) => {
   const { name, cpf, email, sex, birthday, phone } = req.body;
+  console.log(req.body);
 
-  return res
-    .status(200)
-    .json({
-      nome: name,
-      cpf: cpf,
-      email: email,
-      sexo: sex,
-      aniversário: birthday,
-      telefone: phone,
-    });
+  console.log(
+    `nome: ${name} cpf: ${cpf} email: ${email} aniversário: ${birthday} telefone: ${phone}`
+  );
+
+  return res.status(200).json({
+    nome: name,
+    cpf: cpf,
+    email: email,
+    sexo: sex,
+    aniversário: birthday,
+    telefone: phone,
+  });
 };
 
 export default createUserController;
