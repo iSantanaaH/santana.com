e6d53a60d34f0d8b94df4c669141aaa1cd485ee6
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-22 22:30:02 -0300

    create ambient testing im server.js

--- back-end_node/src/server.ts
+++ back-end_node/src/server.ts
@@ -1,6 +1,22 @@
-require("dotenv").config();
-
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-const cors = require("cors");
+import cors from "cors";
+
+
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+
+// Importações das Rotas
+import homeRouter from './routes/homeRouter';
+
+//Uso das Rotas
+app.use("/", homeRouter);
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado em ${new Date()} na porta ${PORT}`);
+});
