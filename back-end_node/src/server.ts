6e3b3fdc963bd3a8bf6343fcb43211ff8ec658e0
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-23 11:16:21 -0300

    remove import dotenv and import with __dirname

--- back-end_node/src/server.ts
+++ back-end_node/src/server.ts
@@ -1,6 +1,4 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-require("dotenv").config();
+require("dotenv").config({ path: __dirname+"/.env" });
 import express from "express";
 import cors from "cors";
 
