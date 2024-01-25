bf23f9f862f099fd65d39f58e4191c3db8fc0346
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-25 15:30:12 -0300

    create refs, states and fuction to validate inputs gender checked and added validate in fuction handleSubmit

--- front-end_next/app/context/cadastroContext.tsx
+++ front-end_next/app/context/cadastroContext.tsx
@@ -16,20 +16,26 @@ interface CadastroContextProps {
   refInputCpf: React.RefObject<HTMLInputElement>;
   refInputEmail: React.RefObject<HTMLInputElement>;
   refInputBirthday: React.RefObject<HTMLInputElement>;
+  refInputGenderMan: React.RefObject<HTMLInputElement>;
+  refInputGenderWoman: React.RefObject<HTMLInputElement>;
+  refInputGenderUninformed: React.RefObject<HTMLInputElement>;
   refInputPhone: React.RefObject<HTMLInputElement>;
   isNameErrorEmpty: string | HTMLInputElement;
   isCpfErrorEmpty: string | HTMLInputElement;
   isEmailErrorEmpty: string | HTMLInputElement;
+  isGenderErrorEmpty: string | HTMLInputElement;
   isBirthdayErrorEmpty: string | HTMLInputElement;
   isPhoneErrorEmpty: string | HTMLInputElement;
   setNameErrorEmpty: Dispatch<SetStateAction<string | HTMLInputElement>>;
   setCpfErrorEmpty: Dispatch<SetStateAction<string | HTMLInputElement>>;
   setEmailErrorEmpty: Dispatch<SetStateAction<string | HTMLInputElement>>;
+  setGenderErrorEmpty: Dispatch<SetStateAction<string | HTMLInputElement>>;
   setBirthdayErrorEmpty: Dispatch<SetStateAction<string | HTMLInputElement>>;
   setPhoneErrorEmpty: Dispatch<SetStateAction<string | HTMLInputElement>>;
   ValidateEmptyInputName: () => void;
   ValidateEmptyInputCpf: () => void;
   ValidateEmptyInputEmail: () => void;
+  ValidateEmptyInputsGender: () => void;
   ValidateEmptyInputBirthday: () => void;
   ValidateEmptyInputPhone: () => void;
   handleSubmit: (event: SyntheticEvent) => void;
@@ -45,6 +51,9 @@ export const CadastroProvider = ({
   const refInputName = useRef<HTMLInputElement | null>(null);
   const refInputCpf = useRef<HTMLInputElement | null>(null);
   const refInputEmail = useRef<HTMLInputElement | null>(null);
+  const refInputGenderMan = useRef<HTMLInputElement | null>(null);
+  const refInputGenderWoman = useRef<HTMLInputElement | null>(null);
+  const refInputGenderUninformed = useRef<HTMLInputElement | null>(null);
   const refInputBirthday = useRef<HTMLInputElement | null>(null);
   const refInputPhone = useRef<HTMLInputElement | null>(null);
   const refFormRegister = useRef<HTMLFormElement | null>(null);
@@ -61,8 +70,10 @@ export const CadastroProvider = ({
   const [isPhoneErrorEmpty, setPhoneErrorEmpty] = useState<
     HTMLInputElement | string
   >("");
-
   const [isNameErrorEmpty, setNameErrorEmpty] = useState<
+    HTMLInputElement | string
+  >("");
+  const [isGenderErrorEmpty, setGenderErrorEmpty] = useState<
     HTMLInputElement | string
   >("");
 
@@ -111,6 +122,26 @@ export const CadastroProvider = ({
     setEmailErrorEmpty(mensagem);
   }
 
+  function ValidateEmptyInputsGender() {
+    const valueInputGenderMan = refInputGenderMan.current?.checked;
+    const valueInputGenderWoman = refInputGenderWoman.current?.checked;
+    const valueInputGenderUninformed =
+      refInputGenderUninformed.current?.checked;
+    let mensagem = "";
+
+    if (
+      valueInputGenderMan ||
+      valueInputGenderWoman ||
+      valueInputGenderUninformed
+    ) {
+      setGenderErrorEmpty("");
+      return;
+    } else {
+      mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
+    }
+    setGenderErrorEmpty(mensagem);
+  }
+
   function ValidateEmptyInputBirthday() {
     const value = refInputBirthday.current?.value.trim();
     let mensagem = "";
@@ -137,6 +168,7 @@ export const CadastroProvider = ({
     ValidateEmptyInputName();
     ValidateEmptyInputCpf();
     ValidateEmptyInputEmail();
+    ValidateEmptyInputsGender();
     ValidateEmptyInputBirthday();
     ValidateEmptyInputPhone();
 
@@ -144,6 +176,7 @@ export const CadastroProvider = ({
       !isNameErrorEmpty &&
       !isCpfErrorEmpty &&
       !isEmailErrorEmpty &&
+      !isGenderErrorEmpty &&
       !isBirthdayErrorEmpty &&
       !isPhoneErrorEmpty
     ) {
@@ -153,11 +186,21 @@ export const CadastroProvider = ({
         const email = refInputEmail.current?.value.trim();
         const birthday = refInputBirthday.current?.value.trim();
         const phone = refInputPhone.current?.value.trim();
+        let gender = "";
+
+        if (refInputGenderMan.current?.checked) {
+          gender = "Masculino";
+        } else if (refInputGenderWoman.current?.checked) {
+          gender = "Feminino";
+        } else if (refInputGenderUninformed.current?.checked) {
+          gender = "Não informado";
+        }
 
         const formData = {
           name,
           cpf,
           email,
+          gender,
           birthday,
           phone,
         };
@@ -168,7 +211,7 @@ export const CadastroProvider = ({
         );
 
         if (response.status === 200) {
-          console.log("Formulário enviado com sucesso!");
+          refFormRegister.current?.reset;
           console.log(response.data);
         } else {
           console.error(`Erro ao enviar o formulário ${response.status}`);
@@ -186,21 +229,27 @@ export const CadastroProvider = ({
         refInputName,
         refInputCpf,
         refInputEmail,
+        refInputGenderMan,
+        refInputGenderWoman,
+        refInputGenderUninformed,
         refInputBirthday,
         refInputPhone,
         isCpfErrorEmpty,
         isEmailErrorEmpty,
+        isGenderErrorEmpty,
         isBirthdayErrorEmpty,
         isPhoneErrorEmpty,
         isNameErrorEmpty,
         setNameErrorEmpty,
         setCpfErrorEmpty,
         setEmailErrorEmpty,
+        setGenderErrorEmpty,
         setBirthdayErrorEmpty,
         setPhoneErrorEmpty,
         ValidateEmptyInputName,
         ValidateEmptyInputCpf,
         ValidateEmptyInputEmail,
+        ValidateEmptyInputsGender,
         ValidateEmptyInputBirthday,
         ValidateEmptyInputPhone,
         handleSubmit,
