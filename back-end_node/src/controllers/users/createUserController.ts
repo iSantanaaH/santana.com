d40a26484c0ef1d3a43ffb3d329cd11b868dd8c5
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-25 15:30:43 -0300

    update controller create user to return for the client correspondent gender

--- back-end_node/src/controllers/users/createUserController.ts
+++ back-end_node/src/controllers/users/createUserController.ts
@@ -1,18 +1,18 @@
 import { Request, Response } from "express";
 
 const createUserController = (req: Request, res: Response) => {
-  const { name, cpf, email, sex, birthday, phone } = req.body;
+  const { name, cpf, email, gender, birthday, phone } = req.body;
   console.log(req.body);
 
   console.log(
-    `nome: ${name} cpf: ${cpf} email: ${email} aniversário: ${birthday} telefone: ${phone}`
+    `nome: ${name} cpf: ${cpf} email: ${email} gênero: ${gender} aniversário: ${birthday} telefone: ${phone}`
   );
 
   return res.status(200).json({
     nome: name,
     cpf: cpf,
     email: email,
-    sexo: sex,
+    sexo: gender,
     aniversário: birthday,
     telefone: phone,
   });
