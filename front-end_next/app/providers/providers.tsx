18ef99f8fe5b1cae317d40a1fe860a8302a8df35
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-27 14:42:50 -0300

    change import useCadastroContext

--- front-end_next/app/providers/providers.tsx
+++ front-end_next/app/providers/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CadastroProvider } from "../context/cadastroContext";
+import { CadastroProvider } from "../context/cadastro/cadastroContext";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
