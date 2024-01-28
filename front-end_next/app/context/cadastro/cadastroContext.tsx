2defddea897940b6a77bfb15f2f1282361687391
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-28 15:05:12 -0300

    refatory CadastroContext.tsx

--- front-end_next/app/context/cadastro/cadastroContext.tsx
+++ front-end_next/app/context/cadastro/cadastroContext.tsx
@@ -4,13 +4,10 @@ import React, {
   useState,
   useRef,
   useContext,
-  Dispatch,
-  SetStateAction,
   SyntheticEvent,
-  ChangeEvent,
 } from "react";
-import { CadastroContextProps } from "./cadastroContextTypes";
 import axios from "axios";
+import { CadastroContextProps } from "./cadastroContextTypes";
 
 export const CadastroContext = createContext({} as CadastroContextProps);
 
@@ -19,174 +16,173 @@ export const CadastroProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const [name, setName] = useState<string>("");
+  const refInputName = useRef<HTMLInputElement | null>(null);
+  const refInputCpf = useRef<HTMLInputElement | null>(null);
+  const refInputEmail = useRef<HTMLInputElement | null>(null);
+  const refInputPasssword = useRef<HTMLInputElement | null>(null);
+  const refInputGenderMan = useRef<HTMLInputElement | null>(null);
+  const refInputGenderWoman = useRef<HTMLInputElement | null>(null);
+  const refInputGenderUninformed = useRef<HTMLInputElement | null>(null);
+  const refInputBirthday = useRef<HTMLInputElement | null>(null);
+  const refInputPhone = useRef<HTMLInputElement | null>(null);
+  const refFormRegister = useRef<HTMLFormElement | null>(null);
 
-  const [isCpfErrorEmpty, setCpfErrorEmpty] = useState<
+  const [isCpfError, setCpfError] = useState<HTMLInputElement | string>("");
+  const [isEmailError, setEmailError] = useState<HTMLInputElement | string>("");
+  const [isPasswordError, setPasswordError] = useState<
     HTMLInputElement | string
   >("");
-  const [isEmailErrorEmpty, setEmailErrorEmpty] = useState<
+  const [isBirthdateError, setBirthdayError] = useState<
     HTMLInputElement | string
   >("");
-  const [isPasswordErrorEmpty, setPasswordErrorEmpty] = useState<
-    HTMLInputElement | string
-  >("");
-  const [isBirthdayErrorEmpty, setBirthdayErrorEmpty] = useState<
-    HTMLInputElement | string
-  >("");
-  const [isPhoneErrorEmpty, setPhoneErrorEmpty] = useState<
-    HTMLInputElement | string
-  >("");
-  const [isNameErrorEmpty, setNameErrorEmpty] = useState<
-    HTMLInputElement | string
-  >("");
-  const [isGenderErrorEmpty, setGenderErrorEmpty] = useState<
-    HTMLInputElement | string
-  >("");
+  const [isPhoneError, setPhoneError] = useState<HTMLInputElement | string>("");
+  const [isNameError, setNameError] = useState<HTMLInputElement | string>("");
+  const [isGenderError, setGenderError] = useState<HTMLInputElement | string>(
+    ""
+  );
 
   const MENSAGEM_CAMPO_OBRIGATORIO = "Campo obrigatório";
 
-  function handleValidateEmptyInputName(
-    event: React.ChangeEvent<HTMLInputElement>
-  ) {
-    const value = event.target.value;
+  function handleValidateName() {
+    const value = refInputName.current?.value.trim();
     let mensagem = "";
 
     if (!value) {
       mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
-    } else {
-      setName(value);
     }
-    setNameErrorEmpty(mensagem);
+    setNameError(mensagem);
   }
 
-  // function FormatCpf(cpf: string): string {
-  //   let valueInputCpf = refInputCpf.current?.value.trim();
-  //   const cleanedCpf = cpf.replace(/\D/g, "");
+  function FormatCpf(cpf: string): string {
+    let valueInputCpf = refInputCpf.current?.value.trim();
+    const cleanedCpf = cpf.replace(/\D/g, "");
 
-  //   return (valueInputCpf = cleanedCpf.replace(
-  //     /(\d{3})(\d{3})(\d{3})(\d{2})/,
-  //     "$1.$2.$3-$4"
-  //   ));
-  // }
+    return (valueInputCpf = cleanedCpf.replace(
+      /(\d{3})(\d{3})(\d{3})(\d{2})/,
+      "$1.$2.$3-$4"
+    ));
+  }
 
-  // function ValidateEmptyInputCpf() {
-  //   const value = refInputCpf.current?.value.trim();
-  //   const Mensagem_CPF_Inválido = "Por favor, insira um CPF válido";
-  //   let mensagem = "";
+  function handleValidateCpf() {
+    const value = refInputCpf.current?.value.trim();
+    const Mensagem_CPF_Inválido = "Por favor, insira um CPF válido";
+    let mensagem = "";
 
-  //   if (!value) {
-  //     mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
-  //   } else if (value.length !== 11) {
-  //     mensagem = Mensagem_CPF_Inválido;
-  //   }
-  //   setCpfErrorEmpty(mensagem);
-  // }
+    if (!value) {
+      mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
+    } else if (value.length < 11) {
+      mensagem = Mensagem_CPF_Inválido;
+    }
+    setCpfError(mensagem);
+  }
 
-  // function ValidateEmptyInputEmail() {
-  //   const value = refInputEmail.current?.value.trim();
-  //   let mensagem = "";
+  function handleValidateEmail() {
+    const value = refInputEmail.current?.value.trim();
+    let mensagem = "";
 
-  //   if (!value) {
-  //     mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
-  //   }
-  //   setEmailErrorEmpty(mensagem);
-  // }
+    if (!value) {
+      mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
+    }
+    setEmailError(mensagem);
+  }
 
-  // function ValidateEmptyInputPassword() {
-  //   const value = refInputPasssword.current?.value.trim();
-  //   const Mensagem_Senha_Inválida = "A senha deve conter no mínimo 12 dígitos";
-  //   let mensagem = "";
+  function handleValidatePassword() {
+    const value = refInputPasssword.current?.value.trim();
+    const Mensagem_Senha_Inválida = "A senha deve conter no mínimo 12 dígitos";
+    let mensagem = "";
 
-  //   if (!value) {
-  //     mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
-  //   } else if (value.length < 12) {
-  //     mensagem = Mensagem_Senha_Inválida;
-  //   }
-  //   setPasswordErrorEmpty(mensagem);
-  // }
+    if (!value) {
+      mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
+    } else if (value.length < 12) {
+      mensagem = Mensagem_Senha_Inválida;
+    }
+    setPasswordError(mensagem);
+  }
 
-  // function ValidateEmptyInputsGender() {
-  //   const valueInputGenderMan = refInputGenderMan.current?.checked;
-  //   const valueInputGenderWoman = refInputGenderWoman.current?.checked;
-  //   const valueInputGenderUninformed =
-  //     refInputGenderUninformed.current?.checked;
-  //   let mensagem = "";
+  function handleValidateGender() {
+    const valueInputGenderMan = refInputGenderMan.current?.checked;
+    const valueInputGenderWoman = refInputGenderWoman.current?.checked;
+    const valueInputGenderUninformed =
+      refInputGenderUninformed.current?.checked;
+    let mensagem = "";
 
-  //   if (
-  //     valueInputGenderMan ||
-  //     valueInputGenderWoman ||
-  //     valueInputGenderUninformed
-  //   ) {
-  //     setGenderErrorEmpty("");
-  //     return;
-  //   } else {
-  //     mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
-  //   }
-  //   setGenderErrorEmpty(mensagem);
-  // }
+    if (
+      valueInputGenderMan ||
+      valueInputGenderWoman ||
+      valueInputGenderUninformed
+    ) {
+      setGenderError("");
+      return;
+    } else {
+      mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
+    }
+    setGenderError(mensagem);
+  }
 
-  // function ValidateEmptyInputBirthday() {
-  //   const value = refInputBirthday.current?.value.trim();
-  //   let mensagem = "";
+  function handleValidateBirthdate() {
+    const value = refInputBirthday.current?.value.trim();
+    let mensagem = "";
 
-  //   if (!value) {
-  //     mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
-  //   }
-  //   setBirthdayErrorEmpty(mensagem);
-  // }
+    if (!value) {
+      mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
+    }
+    setBirthdayError(mensagem);
+  }
 
-  // function ValidateEmptyInputPhone() {
-  //   const value = refInputPhone.current?.value.trim();
-  //   let mensagem = "";
+  function handleValidatePhone() {
+    const value = refInputPhone.current?.value.trim();
+    let mensagem = "";
 
-  //   if (!value) {
-  //     mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
-  //   }
-  //   setPhoneErrorEmpty(mensagem);
-  // }
+    if (!value) {
+      mensagem = MENSAGEM_CAMPO_OBRIGATORIO;
+    }
+    setPhoneError(mensagem);
+  }
 
   async function handleSubmit(event: SyntheticEvent) {
     event.preventDefault();
 
-    // handleValidateEmptyInputName();
-    // ValidateEmptyInputCpf();
-    // ValidateEmptyInputEmail();
-    // ValidateEmptyInputPassword();
-    // ValidateEmptyInputsGender();
-    // ValidateEmptyInputBirthday();
-    // ValidateEmptyInputPhone();
+    handleValidateName();
+    handleValidateCpf();
+    handleValidateEmail();
+    handleValidatePassword();
+    handleValidateGender();
+    handleValidateBirthdate();
+    handleValidatePhone();
 
     if (
-      !isNameErrorEmpty &&
-      !isCpfErrorEmpty &&
-      !isEmailErrorEmpty &&
-      !isGenderErrorEmpty &&
-      !isBirthdayErrorEmpty &&
-      !isPhoneErrorEmpty
+      !isNameError &&
+      !isCpfError &&
+      !isEmailError &&
+      !isGenderError &&
+      !isBirthdateError &&
+      !isPhoneError
     ) {
       try {
-        // const name = refInputName.current?.value.trim();
-        // const cpf = refInputCpf.current?.value.trim();
-        // const email = refInputEmail.current?.value.trim();
-        // const birthday = refInputBirthday.current?.value.trim();
-        // const phone = refInputPhone.current?.value.trim();
+        const name = refInputName.current?.value.trim();
+        const cpf = refInputCpf.current?.value.trim();
+        const email = refInputEmail.current?.value.trim();
+        const password = refInputPasssword.current?.value.trim();
+        const birthdate = refInputBirthday.current?.value.trim();
+        const phone = refInputPhone.current?.value.trim();
         let gender = "";
 
-        // if (refInputGenderMan.current?.checked) {
-        //   gender = "Masculino";
-        // } else if (refInputGenderWoman.current?.checked) {
-        //   gender = "Feminino";
-        // } else if (refInputGenderUninformed.current?.checked) {
-        //   gender = "Não informado";
-        // }
+        if (refInputGenderMan.current?.checked) {
+          gender = "Masculino";
+        } else if (refInputGenderWoman.current?.checked) {
+          gender = "Feminino";
+        } else if (refInputGenderUninformed.current?.checked) {
+          gender = "Não informado";
+        }
 
         const formData = {
           name,
-          // cpf,
-          // email,
-          // gender,
-          // birthday,
-          // phone,
+          cpf,
+          email,
+          password,
+          gender,
+          birthdate,
+          phone,
         };
 
         const response = await axios.post(
@@ -195,7 +191,7 @@ export const CadastroProvider = ({
         );
 
         if (response.status === 200) {
-          // refFormRegister.current?.reset();
+          refFormRegister.current?.reset();
           console.log(response.data);
         } else {
           console.error(`Erro ao enviar o formulário ${response.status}`);
@@ -209,27 +205,37 @@ export const CadastroProvider = ({
   return (
     <CadastroContext.Provider
       value={{
-        isCpfErrorEmpty,
-        isEmailErrorEmpty,
-        isPasswordErrorEmpty,
-        isGenderErrorEmpty,
-        isBirthdayErrorEmpty,
-        isPhoneErrorEmpty,
-        isNameErrorEmpty,
-        setNameErrorEmpty,
-        setCpfErrorEmpty,
-        setEmailErrorEmpty,
-        setPasswordErrorEmpty,
-        setGenderErrorEmpty,
-        setBirthdayErrorEmpty,
-        setPhoneErrorEmpty,
-        handleValidateEmptyInputName,
-        // ValidateEmptyInputCpf,
-        // ValidateEmptyInputEmail,
-        // ValidateEmptyInputPassword,
-        // ValidateEmptyInputsGender,
-        // ValidateEmptyInputBirthday,
-        // ValidateEmptyInputPhone,
+        refFormRegister,
+        refInputName,
+        refInputCpf,
+        refInputEmail,
+        refInputPasssword,
+        refInputGenderMan,
+        refInputGenderWoman,
+        refInputGenderUninformed,
+        refInputBirthday,
+        refInputPhone,
+        isNameError,
+        isCpfError,
+        isEmailError,
+        isPasswordError,
+        isGenderError,
+        isBirthdateError,
+        isPhoneError,
+        setNameError,
+        setCpfError,
+        setEmailError,
+        setPasswordError,
+        setGenderError,
+        setBirthdayError,
+        setPhoneError,
+        handleValidateName,
+        handleValidateCpf,
+        handleValidateEmail,
+        handleValidatePassword,
+        handleValidateGender,
+        handleValidateBirthdate,
+        handleValidatePhone,
         handleSubmit,
       }}
     >
