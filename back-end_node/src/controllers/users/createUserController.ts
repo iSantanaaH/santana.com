9591419e5087c47830dfe07735363cb5a4e6ea2d
Micael Santana <micaelsantana.dev@gmail.com>
2024-01-28 15:04:50 -0300

    update controller create user

--- back-end_node/src/controllers/users/createUserController.ts
+++ back-end_node/src/controllers/users/createUserController.ts
@@ -1,21 +1,28 @@
 import { Request, Response } from "express";
 
 const createUserController = (req: Request, res: Response) => {
-  const { name, cpf, email, gender, birthday, phone } = req.body;
+  const { name, cpf, email, password, gender, birthdate, phone } = req.body;
   console.log(req.body);
 
   console.log(
-    `nome: ${name} cpf: ${cpf} email: ${email} gênero: ${gender} aniversário: ${birthday} telefone: ${phone}`
+    `nome: ${name} cpf: ${cpf} email: ${email} senha: ${password} gênero: ${gender} aniversário: ${birthdate} telefone: ${phone}`
   );
 
-  return res.status(200).json({
-    nome: name,
-    cpf: cpf,
-    email: email,
-    sexo: gender,
-    aniversário: birthday,
-    telefone: phone,
-  });
+  if (!name || !cpf || !email || !password || !gender || !birthdate || !phone) {
+    return res
+      .status(400)
+      .json({ mensagem: "Todos os campos precisam ser preenchidos" });
+  } else {
+    return res.status(200).json({
+      nome: name,
+      cpf: cpf,
+      email: email,
+      password: password,
+      sexo: gender,
+      aniversário: birthdate,
+      telefone: phone,
+    });
+  }
 };
 
 export default createUserController;
