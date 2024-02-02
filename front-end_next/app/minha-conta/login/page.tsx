12ece6e8ec1f2d9d898ab2013e84ffe50fc6e0da
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-02 10:28:33 -0300

    change layout html in page login

--- front-end_next/app/minha-conta/login/page.tsx
+++ front-end_next/app/minha-conta/login/page.tsx
@@ -1,7 +1,55 @@
+import LayoutUser from "@/app/layouts/user";
+import styles from "./login.module.css";
+import Link from "next/link";
+
 export default function LoginPageUser() {
   return (
     <>
-      <h1>Está na rota login</h1>
+      <LayoutUser>
+        <section className={styles.MainContainer}>
+          <div className={styles.ContentTitle}>
+            <article className={styles.ArticleTitle}>
+              <h1>Fazer Login</h1>
+            </article>
+          </div>
+          <form className={styles.FormLogin}>
+            <div className={styles.ContainerOptions}>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                placeholder="Digite seu email"
+                className={styles.InputEmail}
+              />
+              <span className={styles.SpanErrorRender}></span>
+            </div>
+            <div className={styles.ContainerOptions}>
+              <label htmlFor="email">Senha</label>
+              <input
+                type="password"
+                id="password"
+                required
+                placeholder="senha"
+                className={styles.InputPassword}
+              />
+              <span className={styles.SpanInfo}>mínimo 8 caracteres</span>
+              <span className={styles.SpanErrorRender}></span>
+            </div>
+            <div className={styles.ContentSubmit}>
+              <button type="submit">
+                <span>Entrar</span>
+              </button>
+              <p>
+                Não tem conta? <Link href={"/minha-conta/cadastro"}>
+                  <span>Cadastre-se</span>
+                </Link>
+              </p>
+            </div>
+          </form>
+        </section>
+      </LayoutUser>
     </>
   );
 }
