955a3d39df97fbe349d4dfe6318c0978362057ef
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-04 16:54:51 -0300

    added link redirect in image

--- front-end_next/app/layouts/user/index.tsx
+++ front-end_next/app/layouts/user/index.tsx
@@ -1,5 +1,6 @@
 import styles from "./index.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 interface LayoutUserProps {
   children: React.ReactNode;
@@ -10,14 +11,16 @@ export default function LayoutUser({ children }: LayoutUserProps) {
     <main className={styles.MainContainer}>
       <section className={styles.ContentImageBackground}>
         <div>
-          <Image
-            src={"/Images/logo2.svg"}
-            width={50}
-            height={50}
-            alt="Santana.com"
-            priority
-            className={styles.Image}
-          />
+          <Link href={"/"}>
+            <Image
+              src={"/Images/logo2.svg"}
+              width={50}
+              height={50}
+              alt="Santana.com"
+              priority
+              className={styles.Image}
+            />
+          </Link>
         </div>
       </section>
       <section className={styles.SectionChildren}>{children}</section>
