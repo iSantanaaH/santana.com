2aa0b957453e3d4d884496a21334df4ca80c209c
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-06 14:57:56 -0300

    change layout MainNavBar

--- front-end_next/app/components/mainNavBar/index.tsx
+++ front-end_next/app/components/mainNavBar/index.tsx
@@ -7,19 +7,49 @@ export default function MainNavBar() {
     <>
       <section className={styles.NavBar}>
         <div>
-          <div>
-            <Link href={"/"}>
-              <Image
-                src={"/Images/logo8.svg"}
-                width={50}
-                height={50}
-                alt="Santana.com"
-                title="Santana.com"
+          <div className={styles.SectionLogo}>
+            <div>
+              <Link href={"/"}>
+                <Image
+                  src={"/Images/logo.svg"}
+                  width={50}
+                  height={50}
+                  alt="Santana.com"
+                  title="Santana.com"
+                />
+              </Link>
+            </div>
+            <div>
+              <input
+                type="search"
+                placeholder="Buscar marcas, produtos e mais..."
               />
+            </div>
+            <div>
+              {/* Lugar onde será colocado o banner pra ser cliente vip */}
+            </div>
+          </div>
+          <div className={styles.ContainerLinks}>
+            {/* Aqui vai ser a API de CEP pra mostrar o CEP atual da pessoa */}
+            <Link href={"/"}>
+              <span>Categorias</span>
+            </Link>
+            <Link href={"/"}>
+              <span>Contato</span>
+            </Link>
+            <Link href={"/"}>
+              <span>Moda</span>
+            </Link>
+            <Link href={"/"}>
+              <span>Histórico</span>
+            </Link>
+            <Link href={"/"}>
+              <span>Ofertas do dia</span>
+            </Link>
+            <Link href={"/"}>
+              <span></span>
             </Link>
           </div>
-
-          <input type="search" placeholder="busque aqui seu produto" />
 
           <div>
             <Image src={""} width={50} height={50} alt="" />
