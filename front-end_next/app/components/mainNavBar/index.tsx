e97215bc7153c8564401bfcacd20becc0745a3c3
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-08 00:46:30 -0300

    use class LinkDefault in links

--- front-end_next/app/components/mainNavBar/index.tsx
+++ front-end_next/app/components/mainNavBar/index.tsx
@@ -27,46 +27,57 @@ export default function MainNavBar() {
           </div>
           <div className={styles.ContainerLinks}>
             {/* Aqui vai ser a API de CEP pra mostrar o CEP atual da pessoa */}
-            <Link className={styles.LinksInfo} href={"/"}>
+            <Link className={`${"LinkDefault"} ${styles.Category}`} href={"/"}>
               <span>Categorias</span>
             </Link>
-            <Link className={styles.LinksInfo} href={"/"}>
-              <span>Contato</span>
-            </Link>
-            <Link className={styles.LinksInfo} href={"/"}>
+            <Link className={"LinkDefault"} href={"/"}>
               <span>Moda</span>
             </Link>
-            <Link className={styles.LinksInfo} href={"/"}>
-              <span>Histórico</span>
-            </Link>
-            <Link className={styles.LinksInfo} href={"/"}>
+
+            <Link className={"LinkDefault"} href={"/"}>
               <span>Ofertas do dia</span>
             </Link>
-            <Link className={styles.LinksInfo} href={"/"}>
+            <Link className={"LinkDefault"} href={"/"}>
+              <span>Histórico</span>
+            </Link>
+            <Link className={"LinkDefault"} href={"/"}>
+              <span>Contato</span>
+            </Link>
+            <Link className={"LinkDefault"} href={"/"}>
               <span></span>
             </Link>
           </div>
 
           <div className={styles.ContentUser}>
             <div>
-              <span>Usuário</span>
+              <span className={styles.UserInfo}>Usuário</span>
             </div>
             <div>
-              <Link href={"/"}>
-                <span>Favoritos</span>
-              </Link>
-            </div>
-            <div>
-              <Image src={"/Images/sino.svg"} width={20} height={20} alt="Alerts" />
-            </div>
-            <div>
-              <Link href={"/"}>
-                <span>Carrinho</span>
-              </Link>
-            </div>
-            <div>
-              <Link href={"/"}>
+              <Link className={"LinkDefault"} href={"/"}>
                 <span>Baixe o app</span>
+              </Link>
+            </div>
+            <div>
+              <Link className={"LinkDefault"} href={"/"}>
+                <span className={styles.Favorites}>Favoritos</span>
+              </Link>
+            </div>
+            <div>
+              <Image
+                src={"/Images/sino.svg"}
+                width={20}
+                height={20}
+                alt="Alertas"
+              />
+            </div>
+            <div>
+              <Link href={"/"}>
+                <Image
+                  src={"/Images/carringo.svg"}
+                  width={20}
+                  height={20}
+                  alt="Carrinho"
+                />
               </Link>
             </div>
           </div>
