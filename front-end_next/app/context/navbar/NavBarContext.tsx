a3221b27511dad7ffa5c438052a679e7a160842b
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-10 09:41:09 -0300

    add context and add fuction enable and disable dropdown categories

--- front-end_next/app/context/navbar/NavBarContext.tsx
+++ front-end_next/app/context/navbar/NavBarContext.tsx
@@ -1,12 +1,38 @@
-import { createContext } from "react";
-import { NavBarContextPropos } from "./NavBarContextTypes";
+import { createContext, useContext, useState } from "react";
+import { NavBarContextProps } from "./NavBarContextTypes";
 
-export const NavBarContext = createContext({} as NavBarContextPropos);
+export const NavBarContext = createContext({} as NavBarContextProps);
 
 interface NavBarProviderProps {
   children: React.ReactNode;
 }
 
 export const NavBarProvider = ({ children }: NavBarProviderProps) => {
-  return <NavBarContext.Provider value={{}}>{children}</NavBarContext.Provider>;
+  const [isShowCategories, setShowCategories] = useState<
+    boolean | HTMLSpanElement
+  >(false);
+  const [isShowUsers, setShowUsers] = useState<boolean | HTMLSpanElement>(
+    false
+  );
+  const [isShowFavorites, setShowFavorites] = useState<
+    boolean | HTMLSpanElement
+  >(false);
+
+  function ShowCategories() {
+    setShowCategories(true);
+  }
+
+  function handleHiddenCategories() {
+    setShowCategories(false);
+  }
+
+  return (
+    <NavBarContext.Provider
+      value={{ isShowCategories, ShowCategories, handleHiddenCategories }}
+    >
+      {children}
+    </NavBarContext.Provider>
+  );
 };
+
+export const useNavBarContext = () => useContext(NavBarContext);
