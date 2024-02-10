8ddc68bb850f5a377e73deb39bcd8d3ec1469f65
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-10 17:20:57 -0300

    use MainLayoutHome in component HomePage

--- front-end_next/app/page.tsx
+++ front-end_next/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import MainNavBar from "./components/mainNavBar";
+import MainLayoutHome from "./layouts/mainLayout";
 
 export default function HomePage() {
   return (
-    <main className={styles.MainHome}>
-      <section>
-        <MainNavBar />
-      </section>
-    </main>
+    <MainLayoutHome>
+      <h1>Página principal</h1>
+    </MainLayoutHome>
   );
 }
