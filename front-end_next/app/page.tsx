2a19dd9d7fea693f69049c22cb07660c305a298c
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-16 16:27:36 -0300

    create layout carrousel in home page

--- front-end_next/app/page.tsx
+++ front-end_next/app/page.tsx
@@ -1,9 +1,18 @@
 import MainLayoutHome from "./layouts/mainLayout";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
     <MainLayoutHome>
-      <h1>Página principal</h1>
+      <section className={styles.Carrousel}>
+        <div className={styles.LeftCarrousel}></div>
+        <div className={styles.RightCarrousel}></div>
+        <Image className={styles.ImageCarrousel} src={"/Images/logo2.svg"} width={100} height={100} alt="Carrousel Santana.com" />
+        <div className={styles.DivGradient}></div>
+      </section>
+      <section>
+      </section>
     </MainLayoutHome>
   );
 }
