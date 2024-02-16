f0d4d6ae81c8059166a4bc9feab72640f51896e8
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-16 17:01:16 -0300

    use state to show arrows and using function in section in events onmouse enter and onmouseleave

--- front-end_next/app/page.tsx
+++ front-end_next/app/page.tsx
@@ -1,18 +1,40 @@
+"use client";
+import { useHomeContext } from "./context/home/HomeContext";
 import MainLayoutHome from "./layouts/mainLayout";
 import styles from "./page.module.css";
 import Image from "next/image";
 
 export default function HomePage() {
+  const {
+    handleHiddenArrowChangeCarrousel,
+    handleShowArrowChangeCarrousel,
+    showArrowChangeCarrousel,
+  } = useHomeContext();
+
   return (
     <MainLayoutHome>
-      <section className={styles.Carrousel}>
-        <div className={styles.LeftCarrousel}></div>
-        <div className={styles.RightCarrousel}></div>
-        <Image className={styles.ImageCarrousel} src={"/Images/logo2.svg"} width={100} height={100} alt="Carrousel Santana.com" />
+      <section
+        onMouseEnter={handleShowArrowChangeCarrousel}
+        onMouseLeave={handleHiddenArrowChangeCarrousel}
+        className={styles.Carrousel}
+      >
+        {showArrowChangeCarrousel && (
+          <>
+            <div className={styles.LeftCarrousel}></div>
+            <div className={styles.RightCarrousel}></div>
+          </>
+        )}
+
+        <Image
+          className={styles.ImageCarrousel}
+          src={"/Images/logo2.svg"}
+          width={100}
+          height={100}
+          alt="Carrousel Santana.com"
+        />
         <div className={styles.DivGradient}></div>
       </section>
-      <section>
-      </section>
+      <section></section>
     </MainLayoutHome>
   );
 }
