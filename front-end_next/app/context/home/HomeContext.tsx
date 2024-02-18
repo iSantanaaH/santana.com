fee63c130a9cf7c120fe3827e22f97e3117106f5
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-18 12:48:13 -0300

    create function slide to homePage

--- front-end_next/app/context/home/HomeContext.tsx
+++ front-end_next/app/context/home/HomeContext.tsx
@@ -2,6 +2,8 @@
 
 import { createContext, useContext, useState } from "react";
 import { HomeContextProps } from "./HomeContextTypes";
+import styles from "@/app/page.module.css";
+import Image from "next/image";
 
 export const HomeContext = createContext({} as HomeContextProps);
 
@@ -9,9 +11,19 @@ interface HomeProviderProps {
   children: React.ReactNode;
 }
 
+interface SlideShowProps {
+  images: string[];
+}
+
 export const HomeProvider = ({ children }: HomeProviderProps) => {
   const [showArrowChangeCarrousel, setShowArrowChangeCarrousel] =
     useState<boolean>(false);
+  const images = [
+    "/Images/logo2.svg",
+    "/Images/shield_check.svg",
+    "/Images/sino.svg",
+    "/Images/shop.svg",
+  ];
 
   function handleShowArrowChangeCarrousel() {
     setShowArrowChangeCarrousel(true);
@@ -27,6 +39,7 @@ export const HomeProvider = ({ children }: HomeProviderProps) => {
         showArrowChangeCarrousel,
         handleShowArrowChangeCarrousel,
         handleHiddenArrowChangeCarrousel,
+        images,
       }}
     >
       {children}
@@ -35,3 +48,40 @@ export const HomeProvider = ({ children }: HomeProviderProps) => {
 };
 
 export const useHomeContext = () => useContext(HomeContext);
+
+export default function SlideHomePage({ images }: SlideShowProps) {
+  const { showArrowChangeCarrousel } = useHomeContext();
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  function NextImageSlide() {
+    const nextIndex = (currentImageIndex + 1) % images.length;
+    setCurrentImageIndex(nextIndex);
+  }
+
+  function PreviousImageSlide() {
+    const previousImageIndex =
+      (currentImageIndex - 1 + images.length) % images.length;
+    setCurrentImageIndex(previousImageIndex);
+  }
+
+  return (
+    <>
+      {showArrowChangeCarrousel && (
+        <>
+          <div
+            onClick={PreviousImageSlide}
+            className={styles.LeftCarrousel}
+          ></div>
+          <div onClick={NextImageSlide} className={styles.RightCarrousel}></div>
+        </>
+      )}
+      <Image
+        src={images[currentImageIndex]}
+        className={styles.ImageCarrousel}
+        width={100}
+        height={100}
+        alt=""
+      />
+    </>
+  );
+}
