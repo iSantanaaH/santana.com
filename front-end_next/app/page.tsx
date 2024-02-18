b1069530f268450256f1302797e7a35316d85b79
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-18 12:46:50 -0300

    calling SlideHomePage in homePage

--- front-end_next/app/page.tsx
+++ front-end_next/app/page.tsx
@@ -2,13 +2,13 @@
 import { useHomeContext } from "./context/home/HomeContext";
 import MainLayoutHome from "./layouts/mainLayout";
 import styles from "./page.module.css";
-import Image from "next/image";
+import SlideHomePage from "./context/home/HomeContext";
 
 export default function HomePage() {
   const {
     handleHiddenArrowChangeCarrousel,
     handleShowArrowChangeCarrousel,
-    showArrowChangeCarrousel,
+    images,
   } = useHomeContext();
 
   return (
@@ -18,25 +18,10 @@ export default function HomePage() {
         onMouseLeave={handleHiddenArrowChangeCarrousel}
         className={styles.Carrousel}
       >
-        {showArrowChangeCarrousel && (
-          <>
-            <div className={styles.LeftCarrousel}></div>
-            <div className={styles.RightCarrousel}></div>
-          </>
-        )}
-
-        <Image
-          className={styles.ImageCarrousel}
-          src={"/Images/logo2.svg"}
-          width={100}
-          height={100}
-          alt="Carrousel Santana.com"
-        />
+        <SlideHomePage images={images} />
         <div className={styles.DivGradient}></div>
       </section>
-      <section>
-        
-      </section>
+      <section></section>
     </MainLayoutHome>
   );
 }
