327ae7c26c1774efa7a8f1b00657728b9781c59f
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-21 15:15:31 -0300

    create schema and migration prisma

---
+++ back-end_node/prisma/migrations/20240221010853_santana_com_db/migration.sql
@@ -0,0 +1,72 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "birthdate" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "photo" TEXT,
+    "number_house" TEXT,
+    "city_id" TEXT,
+    "state_id" TEXT,
+    "country_id" TEXT,
+    "zip_code" TEXT,
+    "street" TEXT,
+    "district" TEXT,
+    "status" BOOLEAN NOT NULL,
+    "permission" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "city" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "codigo" TEXT NOT NULL,
+    "Uf" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "city_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "state" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "ufCode" TEXT NOT NULL,
+    "Uf" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "state_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "country" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "country_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_city_id_fkey" FOREIGN KEY ("city_id") REFERENCES "city"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_state_id_fkey" FOREIGN KEY ("state_id") REFERENCES "state"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_country_id_fkey" FOREIGN KEY ("country_id") REFERENCES "country"("id") ON DELETE SET NULL ON UPDATE CASCADE;
