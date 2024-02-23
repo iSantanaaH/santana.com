1f0000816bc5467f35849b6baa1115bc6bc9e9d0
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-22 22:02:07 -0300

    change block try in middleware

--- back-end_node/src/middlewares/isAuthenticated.ts
+++ back-end_node/src/middlewares/isAuthenticated.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+const jwt = require("jsonwebtoken");
 
 export function isAuthenticated(
   req: Request,
@@ -15,4 +16,11 @@ export function isAuthenticated(
   }
 
   const token = authToken.split("");
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decodedToken);
+  } catch (error) {
+    return res.status(401).end();
+  }
 }
