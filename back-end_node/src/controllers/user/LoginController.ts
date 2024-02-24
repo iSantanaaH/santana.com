ce72d198b56fd7ae250aa5deb63de6806d40760d
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-24 11:39:50 -0300

    change login controller to take request and process in database to verify data and generate token with jsonwebtoken to send token in response

--- back-end_node/src/controllers/user/LoginController.ts
+++ back-end_node/src/controllers/user/LoginController.ts
@@ -1,14 +1,35 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+const prisma = new PrismaClient();
 
-function LoginController(req: Request, res: Response) {
+export default async function LoginController(req: Request, res: Response) {
   const { email, password } = req.body;
-  console.log(req.body);
 
   if (!email || !password) {
-    return res.status(400).json({ mensagem: "Existem campos vazios!" });
-  } else {
-    return res.status(200).json({ email: email, password: password });
+    return res.status(400).json({ error: "Preencha todos os campos" });
+  }
+
+  try {
+    const authUser = await prisma.user.findUnique({
+      where: {
+        email: email,
+        password: password,
+      },
+    });
+
+    if (!authUser) {
+      return res.status(401).json({ error: "Email ou senha inválidos" });
+    }
+
+    const token = jwt.sign(
+      { userId: authUser.id, user_name: authUser.name },
+      process.env.JWT_SECRET as string
+    );
+    return res
+      .status(200)
+      .json({ message: "Sucesso ao realizar login", token: token });
+  } catch (error) {
+    return res.status(400).end();
   }
 }
-
-export default LoginController;
