7be0a1356b151bff3265732509698949c57809dc
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-24 19:57:27 -0300

    create state token and userName, create useEffect to take token and stract user_name to set in state userName

--- front-end_next/app/context/navbar/NavBarContext.tsx
+++ front-end_next/app/context/navbar/NavBarContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, useContext, useRef, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { NavBarContextProps } from "./NavBarContextTypes";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export const NavBarContext = createContext({} as NavBarContextProps);
 
@@ -8,6 +9,8 @@ interface NavBarProviderProps {
 }
 
 export const NavBarProvider = ({ children }: NavBarProviderProps) => {
+  const [token, setToken] = useState<string | boolean>(false);
+  const [userName, setUserName] = useState<string>("");
   const [isShowCategories, setShowCategories] = useState<
     boolean | HTMLSpanElement
   >(false);
@@ -17,6 +20,36 @@ export const NavBarProvider = ({ children }: NavBarProviderProps) => {
   const [isShowFavorites, setShowFavorites] = useState<
     boolean | HTMLSpanElement
   >(false);
+
+  useEffect(() => {
+    // Obtem todos os cookies presentes na página individualmente.
+    const cookies = document.cookie.split(";");
+    let token = null;
+
+    // Percorre os cookies usando o forEach.
+    cookies.forEach((cookie) => {
+      // Seleciona o cookies atual e remove todos os espaços em branco.
+      const trimmedCookie = cookie.trim();
+      if (trimmedCookie.startsWith("santana.com.token=")) {
+        /* Se o token começar com "santana.com.token=" pego o valor
+          dele e coloco dentro da variável "token".
+          */
+        token = trimmedCookie.substring(
+          "santana.com.token=".length,
+          trimmedCookie.length
+        );
+        setToken(token);
+      }
+    });
+
+    if (token) {
+      try {
+        const decodedToken = jwt.decode(token) as JwtPayload;
+        const user_name = decodedToken?.user_name;
+        setUserName(user_name);
+      } catch (error) {}
+    }
+  }, []);
 
   function ShowCategories() {
     setShowCategories(true);
@@ -29,6 +62,8 @@ export const NavBarProvider = ({ children }: NavBarProviderProps) => {
   return (
     <NavBarContext.Provider
       value={{
+        token,
+        userName,
         isShowCategories,
         ShowCategories,
         handleHiddenCategories,
