2c51e1fb525169c27a713193579363585c087822
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-24 19:56:35 -0300

    tiping userName and token

--- front-end_next/app/context/navbar/NavBarContextTypes.ts
+++ front-end_next/app/context/navbar/NavBarContextTypes.ts
@@ -1,6 +1,8 @@
 import { SetStateAction } from "react";
 
 export interface NavBarContextProps {
+  userName: string;
+  token: string | boolean;
   ShowCategories: () => void;
   handleHiddenCategories: () => void;
   isShowCategories: SetStateAction<boolean | HTMLSpanElement>;
