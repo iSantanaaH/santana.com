364a3bdc5add05ee9840d4f1ff3eb104fb9fee21
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-26 18:43:16 -0300

    remove import prisma, const client, import home router and use home router

--- back-end_node/src/server.ts
+++ back-end_node/src/server.ts
@@ -2,12 +2,10 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 import express from "express";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client";
 
 // Declarações principais.
 const app = express();
 const port = process.env.PORT;
-const client = new PrismaClient();
 
 // Uses principais da aplicação.
 app.use(express.json());
@@ -15,12 +13,10 @@ app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.urlencoded({ extended: true }));
 
 // Importações das Rotas.
-import homeRouter from "./routes/homeRouter";
 import CreateUserRouter from "./routes/user/CreateUserRouter";
 import LoginRouter from "./routes/user/LoginRouter";
 
 // Redirecionamento das Rotas.
-app.use("/", homeRouter);
 app.use("/minhaconta/cadastro", CreateUserRouter);
 app.use("/minhaconta/login", LoginRouter);
 
