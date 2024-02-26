9c50af804f2b1e6daa4ed39601793aabc98a32de
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-26 15:29:14 -0300

    delete home router and home controller

--- back-end_node/src/routes/homeRouter.ts
+++
@@ -1,9 +0,0 @@
-// routes/homeRouter.ts
-import express from 'express';
-import homeController from '../controllers/homeController';
-
-const router = express.Router();
-
-router.post("/", homeController);
-
-export default router;
