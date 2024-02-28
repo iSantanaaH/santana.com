7279f236b6fdf3ac2e1ac7791dda91f2003c96e9
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-27 22:39:57 -0300

    format with prettier

--- front-end_next/app/components/footerMainLayout/index.tsx
+++ front-end_next/app/components/footerMainLayout/index.tsx
@@ -8,16 +8,22 @@ export default function FooterMainLayout() {
     <>
       <section className={styles.MainContainerFooter}>
         <div className={styles.PaymentMethods}>
-            <Image src={CreditCardSVG} width={300} height={100} alt="Cartão de Cŕedito" />
+          <Image
+            src={CreditCardSVG}
+            width={300}
+            height={100}
+            alt="Cartão de Cŕedito"
+            priority
+          />
           <h3>Formas de Pagamento</h3>
           <p>
             Você pode pagar com cartão, boleto ou pix. Escolha a mais favorável
             a você. Boas compras!
           </p>
         </div>
-          <div className={styles.BorderDivs}></div>
+        <div className={styles.BorderDivs}></div>
         <div className={styles.Security}>
-        <Image src={ShieldCheck} width={300} height={100} alt="Segurança" />
+          <Image src={ShieldCheck} width={300} height={100} alt="Segurança" />
           <h3>100% Seguro</h3>
           <p>
             Na Santana.com você sempre estará protegido. Caso não tenha gostado
