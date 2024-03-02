14f25ebe9e4e57848f1155d14a85d31f965dcc0e
Micael Santana <micaelsantana.dev@gmail.com>
2024-03-02 12:23:53 -0300

    use isPublicRouterPrrovider in providers to use context

--- front-end_next/app/providers/providers.tsx
+++ front-end_next/app/providers/providers.tsx
@@ -4,17 +4,20 @@ import { CadastroProvider } from "../context/user/cadastro/CadastroContext";
 import { LoginProvider } from "../context/user/login/LoginContext";
 import { NavBarProvider } from "../context/navbar/NavBarContext";
 import { HomeProvider } from "../context/home/HomeContext";
+import { IsPublicRouterProvider } from "../context/isPublicRouter";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <HomeProvider>
-        <NavBarProvider>
-          <LoginProvider>
-            <CadastroProvider>{children}</CadastroProvider>
-          </LoginProvider>
-        </NavBarProvider>
-      </HomeProvider>
+      <IsPublicRouterProvider>
+        <HomeProvider>
+          <NavBarProvider>
+            <LoginProvider>
+              <CadastroProvider>{children}</CadastroProvider>
+            </LoginProvider>
+          </NavBarProvider>
+        </HomeProvider>
+      </IsPublicRouterProvider>
     </>
   );
 };
