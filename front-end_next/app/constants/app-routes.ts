5caceb2dc7f3041fc802a0e4e3561afd3e62ef38
Micael Santana <micaelsantana.dev@gmail.com>
2024-03-02 12:21:23 -0300

    create constant APP_ROUTES private and public

---
+++ front-end_next/app/constants/app-routes.ts
@@ -0,0 +1,39 @@
+export const APP_ROUTES = {
+  private: {
+    profile: {
+      name: "/minha_conta/perfil",
+    },
+    favorites: {
+      name: "/minha_conta/favoritos",
+    },
+    historic: {
+      name: "/minha_conta/historico",
+    },
+    unauthorized: {
+      name: "/unauthorized",
+    },
+  },
+  public: {
+    home: {
+      name: "/",
+    },
+    signIn: {
+      name: "/login",
+    },
+    register: {
+      name: "/cadastro",
+    },
+    contact: {
+      name: "/contato",
+    },
+    cart: {
+      name: "/carrinho",
+    },
+    app: {
+      name: "/santana/app",
+    },
+    offers: {
+      name: "/ofertas_do_dia",
+    },
+  },
+};
