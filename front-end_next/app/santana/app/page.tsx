9a75be474b538a0059ae0eefe36757fe8366a783
Micael Santana <micaelsantana.dev@gmail.com>
2024-03-18 17:42:17 -0300

    change layout santana/app

--- front-end_next/app/santana/app/page.tsx
+++ front-end_next/app/santana/app/page.tsx
@@ -8,18 +8,34 @@ export default function SantanaApp() {
       <header className={styles.Header}>
         <section className={styles.SectionRedirect}>
           <div className={styles.LogoSantana}>
-            <h1>APP Santana.com</h1>
+            <h1 className={styles.Name}>
+              Santana
+              <span className={styles.Domain}>.com</span>
+            </h1>
             <Link href={"/"}>
               <Image src={""} width={50} height={50} alt="" />
             </Link>
           </div>
           <div className={styles.ButtonApp}>
             <p>Descontos exclusivos somente no APP!</p>
-            <button>BAIXE AGORA!</button>
+            <button>
+              <span>BAIXE AGORA</span>
+            </button>
           </div>
         </section>
       </header>
-      <main className={styles.Main}></main>
+      <main className={styles.Main}>
+        <section>
+          <div>
+            <h1>
+              Guia para instalação do APP
+            </h1>
+          </div>
+          <div>
+            <video className={styles.VideoGuide} src=""></video>
+          </div>
+        </section>
+      </main>
       <footer className={styles.Footer}></footer>
     </>
   );
