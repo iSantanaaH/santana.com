d463816be43524244a2a50c10d5d766826f67c45
Micael Santana <micaelsantana.dev@gmail.com>
2024-03-19 17:10:39 -0300

    change section InfoAppUser, add article and Images, change layout Footer

--- front-end_next/app/santana/app/page.tsx
+++ front-end_next/app/santana/app/page.tsx
@@ -30,10 +30,15 @@ export default function SantanaApp() {
             <h1>
               Confira nosso Guia para<br></br>instalação do APP ao lado.
             </h1>
+            <div className={styles.DivBorder}></div>
             <video
+              controls
               className={styles.VideoGuide}
               src="/Videos/teste.mp4"
-            ></video>
+              typeof="video/mp4"
+            >
+              Seu navegador não suporta o elemento de vídeo.
+            </video>
           </div>
         </section>
         <section className={styles.AboutSantanaApp}>
@@ -41,18 +46,64 @@ export default function SantanaApp() {
             <h2>Quais as vantagens de ter o nosso APP?</h2>
           </div>
           <div className={styles.InfoAppForUser}>
-            <h3>Acompanhe seus produtos em tempo real</h3>
-            <div></div>
-            <h3>Avisaremos quando houver alguma promoção</h3>
-            <div></div>
-            <h3>Concorra a cupons de descontos em compras</h3>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+              <h3>Acompanhe seus produtos em tempo real</h3>
+              <article>
+                <Image
+                  src={"/Images/caminhao-de-entrega.svg"}
+                  width={100}
+                  height={100}
+                  alt="Rastreio de produto"
+                  className={styles.ImageArticle}
+                />
+              </article>
+              <p>O APP te dá o poder de rastrear seus produtos em tempo real</p>
+            </div>
+            <div>
+              <h3>Avisaremos quando houver alguma promoção</h3>
+              <article>
+              <Image
+                  src={"/Images/alerta.svg"}
+                  width={100}
+                  height={100}
+                  alt="Rastreio de produto"
+                  className={styles.ImageArticle}
+                />
+              </article>
+              <p>
+                Fique atento no alertas, você será avisado caso alguma promoção
+                esteja ativa
+              </p>
+            </div>
+            <div>
+              <h3>Concorra a cupons de descontos em compras</h3>
+              <article>
+              <Image
+                  src={"/Images/cupom.svg"}
+                  width={100}
+                  height={100}
+                  alt="Rastreio de produto"
+                  className={styles.ImageArticle}
+                />
+              </article>
+              <p>
+                Com o APP você concorre a cupons de desconto para usar em suas
+                compras
+              </p>
+            </div>
           </div>
         </section>
+        <section>
+
+        </section>
       </main>
-      <footer className={styles.Footer}></footer>
+      <footer className={styles.Footer}>
+        <section className={styles.DirectsReserved}>
+          <div>
+            <p>&copy; 2024 Santana.com | Todos os direitos reservados.</p>
+          </div>
+        </section>
+      </footer>
     </>
   );
 }
